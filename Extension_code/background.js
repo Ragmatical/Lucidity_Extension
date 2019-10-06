e2db6efaa61fccc5924930a0710cf53a5d6c9d5e
Justin Hou <sfdudejustin@gmail.com>
2019-10-06 12:04:21 -0700

    blocked sites

--- Extension_code/background.js
+++ Extension_code/background.js
@@ -1,7 +1,49 @@
+var mode = 0;
+var whitelist = ["https://www.schoology.com", "https://www.desmos.com/"]
+
+var blacklist = ["www.google.com", "www.youtube.com"]
+
 chrome.runtime.onMessage.addListener(
     function(req, sender, sendResponse){
-        console.log(req)
+        var url = req.site;
+        if (mode === 0) {
+            if (whitelist.some(el => url.includes(el))){
+                return
+            } else{
+                // console.log(url)
+                sendResponse({res: 'BLOCK'})
+            }
+        } if (mode === 1) {
+            if (blacklist.some(el => url.includes(el))){
+                sendResponse({res: 'BLOCK'})
+            }
+        } if (mode === 2) {
+            return
+        } if (mode === 3) {
+            if(blacklist.some(el => url.includes(el))){
+                sendResponse({res: 'BLOCK'})
+            } if(!blacklist.some(el => url.includes(el)) && !whitelist.some(el => url.includes(el))){
+                sendResponse({res: 'AI'})
+            }
+        }
+
+
     }
-    
-    )
-    console.log('hi')
+)
+// function closeTabs(){
+//     chrome.tabs.query({}, function (tabs) {
+//         for (var i = 0; i < tabs.length; i++) {
+//             if(tabs[i].title == "Off Task!"){
+//                 chrome.tabs.remove(tabs[i].id, null);
+//             }
+//         }
+//     });
+// }
+
+// chrome.runtime.onMessage.addListener(
+//     function(req, sender, sendResponse) {
+//         if (req.subject == "close tab") {
+//             closeTabs();
+//         }
+//     }
+// )
--- Extension_code/content.js
+++ Extension_code/content.js
@@ -1,4 +1,22 @@
 console.log("Page pwdhas finished loading.")
 
 var body = document.body.innerHTML
-chrome.runtime.sendMessage(chrome.runtime.id, {site: location.href}, function(response) {})
+chrome.runtime.sendMessage(chrome.runtime.id, {site: location.href}, function(response) {
+    console.log(response)
+    if(!response) return;
+    
+    if (response.res == "BLOCK" && response.res !== "power off")  {
+        document.write('<!DOCTYPE html><html><head></head><body></body></html>');
+        window.document.title = "You shouldn't be here"
+        
+        document.body.innerHTML = `<p>Go to an educational site!</p><br><button id = "close"> Go Back </button>`;
+        var close = document.getElementById('close')
+        
+        close.addEventListener("click", function(){
+            history.back()
+        })
+        
+        
+        
+    } 
+})
