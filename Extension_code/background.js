489c8af8104104402679e275a323c77250ffe819
Justin Hou <sfdudejustin@gmail.com>
2019-10-06 09:13:19 -0700

    backgroundjs

---
+++ Extension_code/background.js
@@ -0,0 +1,7 @@
+chrome.runtime.onMessage.addListener(
+    function(req, sender, sendResponse){
+        console.log(req)
+    }
+    
+    )
+    console.log('hi')
--- Extension_code/content.js
+++ Extension_code/content.js
@@ -1,29 +1,4 @@
-console.log("Page has finished loading.")
+console.log("Page pwdhas finished loading.")
 
 var body = document.body.innerHTML
-
-function sendData(data) {
-			// Creating a new request to the server that contains the data
-			var req = new XMLHttpRequest();
-
-			// Opening up a connection with the server to 'POST' data
-				req.open('POST', 'http://localhost:8081', true);
-
-			// Explain to the server that we are sending the data in json format
-				req.setRequestHeader('content-type', 'application/json')
-				// Alert the user to any errors from the server
-				function readyStateChange() {
-					if(req.readyState !== 4) return;
-					if(req.status !== 200) return;
-					data = JSON.parse(req.responseText);
-					if(data.error) {
-						alert(data.error);
-					}
-				}
-req.onreadystatechange = readyStateChange;
-
-			// Sending the data in JSON format
-				req.send(JSON.stringify({data}));
-			}
-
-sendData(body)
+chrome.runtime.sendMessage(chrome.runtime.id, {site: location.href}, function(response) {})
