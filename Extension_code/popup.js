7053aa8ec15790679373b572416b50486582a31f
Juno Kim <junokimzone@gmail.com>
2019-11-10 12:21:22 -0800

    Making Tabs
    Need help on putting icons
    Finished login process

--- Extension_code/popup.js
+++ Extension_code/popup.js
@@ -1,28 +1,45 @@
 
 
-document.getElementById("settingsTab").addEventListener('click', function(e) {
-  document.getElementById("settings").style.visibility = "visible";
-  document.getElementById("whitelistPanel").style.visibility = "hidden";
-  document.getElementById("home").style.visibility = "hidden";
-})
-
-document.getElementById("whitelistTab").addEventListener('click', function(e) {
-  document.getElementById("settings").style.visibility = "hidden";
-  document.getElementById("whitelistPanel").style.visibility = "visible";
-  document.getElementById("home").style.visibility = "hidden";
-})
-
-document.getElementById("homeTab").addEventListener('click', function(e) {
-  document.getElementById("settings").style.visibility = "hidden";
-  document.getElementById("whitelistPanel").style.visibility = "hidden";
-  document.getElementById("home").style.visibility = "visible";
-})
+// document.getElementById("settingsTab").addEventListener('click', function(e) {
+//   document.getElementById("settings").style.visibility = "visible";
+//   document.getElementById("whitelistPanel").style.visibility = "hidden";
+//   document.getElementById("home").style.visibility = "hidden";
+// })
+//
+// document.getElementById("whitelistTab").addEventListener('click', function(e) {
+//   document.getElementById("settings").style.visibility = "hidden";
+//   document.getElementById("whitelistPanel").style.visibility = "visible";
+//   document.getElementById("home").style.visibility = "hidden";
+// })
+//
+// document.getElementById("homeTab").addEventListener('click', function(e) {
+//   document.getElementById("settings").style.visibility = "hidden";
+//   document.getElementById("whitelistPanel").style.visibility = "hidden";
+//   document.getElementById("home").style.visibility = "visible";
+// })
 
 // var blacklist = document.querySelector('#blacklist')
 // var whitelist = document.querySelector('#whitelist')
 var username = document.querySelector('#username')
 var password = document.querySelector('#password')
 var loginBtn = document.querySelector('#loginBtn')
+var loginStatus = false;
+
+function openTab(tabName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+  document.getElementById(tabName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+document.getElementById("defaultOpen").click();
 
 chrome.storage.sync.get(['id'], function(result){
 	if(!result.id){
@@ -67,22 +84,24 @@ function convertLists(data){
 
 function sendLoginData(data){
 	var xhr = new XMLHttpRequest();
-  console.log('here')
-	xhr.open('POST', 'https://lucidity.ninja/users/login');
-  console.log('here')
+  console.log("made request")
+	xhr.open('POST', 'https://www.lucidity.ninja/users/login');
+  console.log('posted')
 	xhr.setRequestHeader('content-type', 'application/json');
-  console.log('here')
 	xhr.onreadystatechange = (res) => {
 			if (xhr.readyState != 4 || xhr.status > 300) {
                 return;
             }
-            console.log(xhr,responseText)
+            console.log("response text", xhr.responseText)
         var data = JSON.parse(xhr.responseText);
-        chrome.storage.sync.set({id: data.id})
+        chrome.storage.sync.set({id: data._id}, function(){
+          console.log("data id: ", data._id)
+        })
 				console.log("logged in")
     };
-  console.log(data)
+  console.log("data: ", data)
   xhr.send(JSON.stringify(data));
+  loginStatus = true
 }
 
 loginBtn.addEventListener('click', function(){
