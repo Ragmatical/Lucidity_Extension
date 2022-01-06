4686a6e2ddad82e6783f986f9725d990fdc2474c
boovines <37782339+boovines@users.noreply.github.com>
2022-01-05 19:59:10 -0800

    small adblocker update

--- Extension_code/block.js
+++ Extension_code/block.js
@@ -1,25 +1,22 @@
 function removeAds() {
     // hardcoded bad sites
-    let adDomains = ["adservice.google.com",  "beap.gemini.yahoo.com", "googlesyndication.com"]//"google.com", "yahoo.com",
+    let adIframeIDs =  ["ob_iframe", "google_ads_iframe_", "demdex.net", "google_esf"]//"google.com", "yahoo.com",
+    let adIframeSRCs = ["googlesyndication.com", "demdex.net", "criteo.com", "googleads.g.doubleclick", "ads.pubmatic.com", "outbrain.com"]
+    let adIMGSRCs = ["adservice.google.com"]
+    let adHREFs = ["beap.gemini.yahoo.com"]
     /* interesting words:
     GoogleCreativeContainerClass, div
-    google_ads_iframe_/22152718,16833175/sws-hb//w3schools.com//,
-    google_ads_iframe_/22152718,16833175/sws-hb//w3schools.com//main_leaderboard_1__container__,
-    id="google_ads_iframe_/22152718,16833175/sws-hb//w3schools.com//right_bottom_medium_rectangle_0__container__"
     GoogleActiveViewElement, div
     https://cdn.flashtalking.com/110416/3687032/index.html,
     https://pagead2.googlesyndication.com/pagead/s/cookie_push_onload.html,
     https://b4cb96cea66212361030d6677edbd11b.safeframe.googlesyndication.com/safeframe/1-0-38/html/container.html
     */
+
     // get domain of site
     var url = window.location.href;
-    // check for href starting with "/"
     let parsedUrl = url.replace("https://", "")
                 .replace("http://", "")
                 .replace("www.", "")
-
-    // Remove path and queries e.g. linkedin.com/feed or linkedin.com?query=value
-    // We only want the base domain
     let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/'))
         .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
 
@@ -35,7 +32,7 @@ function removeAds() {
     // Get all 'span' elements on the page
     let iframes = document.getElementsByTagName("iframe");
 
-    for (let i = 0; i < iframe.length; ++i) {
+    for (let i = 0; i < iframe.length; i++) {
         // Check if they contain the text 'Promoted'
         var badlink = iframes[i].src;
         if (adDomains.some(el => badlink.includes(el)) || !badlink.includes(domain) || badlink.charAt(0)!="/") {
