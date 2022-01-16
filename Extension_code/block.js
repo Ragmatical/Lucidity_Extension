41a26de99f39245586c0b63e2f1cbc4340a3cc01
boovines <37782339+boovines@users.noreply.github.com>
2022-01-16 02:19:27 -0800

    block done

--- Extension_code/block.js
+++ Extension_code/block.js
@@ -3,7 +3,7 @@ function removeAds() { // WHITELIST LUCIDITY
     let adIframeIDs =  ["ob_iframe", "google_ads_iframe_", "demdex.net", "google_esf"]//"google.com", "yahoo.com",
     let adIframeSRCs = ["googlesyndication.com", "demdex.net", "criteo.com", "googleads.g.doubleclick", "ads.pubmatic.com", "outbrain.com"]
     let adIMGSRCs = ["adservice.google.com"]
-    let adHREFs = ["beap.gemini.yahoo.com"]
+    let adHREFs = ["beap.gemini.yahoo.com", "yahoo.com"]
     /* interesting words:
     GoogleCreativeContainerClass, div
     GoogleActiveViewElement, div
@@ -21,15 +21,28 @@ function removeAds() { // WHITELIST LUCIDITY
         .slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
 
 
-    // // hide all <a> parents by href
+    // hide all <a> parents by href
     // var links = [], links = document.links;
     // for(var i=0; i<links.length; i++) {
     //   if(adHREFs.some(el => links[i].href.includes(el)) || !links[i].href.includes(domain) || links[i].href.charAt(0)!="/"){
-    //     let card = links[i].closest("div");
-    //     makedisappear(card, "a");
+    //     let card = links[i].closest("a");
+    //     makedisappear(card, "div");
     //   }
     // }
+// a.forEach(el => addas.append((el.href.includes(url)) ? "" : el.href))
+    var a = document.getElementsByTagName("a");
+    var addas = [];
+    var url = "usatoday";
 
+    for(var i=0; i<a.length; i++){
+      var d = a[i].href;
+      if(adHREFs.some(el => d.includes(el)) || !d.includes(url)){
+        if(a[i].title != ""){
+          let card = a[i].closest("div");
+          makedisappear(card, "div");
+        }
+      }
+    }
     // Get all 'iframe' elements on the page
     let iframes = document.getElementsByTagName("iframe");
 
@@ -42,32 +55,55 @@ function removeAds() { // WHITELIST LUCIDITY
                 // Get the div that wraps around the entire ad
                 // let card = iframes[i].closest("div");
 
-                makedisappear(iframes[i], "div");
+                iframesmakedisappear(iframes[i], "div");
 
             }
 
         }
     }
 
-    let imgs = document.getElementsByTagName("img");
-
-    for (let i = 0; i < imgs.length; i++) {
-        // Check if they contain the text 'Promoted'
-        var badlink = imgs[i].src;
-        if (adIMGSRCs.some(el => badlink.includes(el)) || !badlink.includes(domain) || badlink.charAt(0)!="/") {
-            // Get the div that wraps around the entire ad
-            // let card = imgs[i].closest("div");
-
-            makedisappear(imgs[i], "div");
-
-
-        }
-    }
+    // let imgs = document.getElementsByTagName("img");
+    //
+    // for (let i = 0; i < imgs.length; i++) {
+    //     // Check if they contain the text 'Promoted'
+    //     var badlink = imgs[i].src;
+    //     if (adIMGSRCs.some(el => badlink.includes(el)) || !badlink.includes(domain) || badlink.charAt(0)!="/") {
+    //         // Get the div that wraps around the entire ad
+    //         // let card = imgs[i].closest("div");
+    //
+    //         makedisappear(imgs[i], "div");
+    //
+    //
+    //     }
+    // }
 }
 
+function iframesmakedisappear(card, tagName){
+    if(card == card.closest(tagName)){
+        var parent = card.parentNode;
+        if(parent != null){
+          var parent2 = parent.parentNode;
+          if (parent2 != null)
+            parent2.setAttribute("style", "display: none !important;");
+        } else{
+          card.setAttribute("style", "display: none !important;");
+        }
+
+    } else {
+        var newcard = card.closest(tagName);
+        console.log(newcard);
+        if(newcard == null)
+          card.setAttribute("style", "display: none !important;");
+        else
+          iframesmakedisappear(newcard, tagName);
+    }
+}
 function makedisappear(card, tagName){
     if(card == card.closest(tagName)){
-        card.setAttribute("style", "display: none !important;");
+        var parent = card.parentNode;
+        if(parent != null){
+          card.setAttribute("style", "display: none !important;");
+        }
 
     } else {
         var newcard = card.closest(tagName);
